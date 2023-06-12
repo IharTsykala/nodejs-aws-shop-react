7a467635272e68923235a329ca89215a54f68772
IharTsykala <ihatsykala@kitrum.com>
2023-06-12 21:13:23 +0600

    fix: api and schema

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://ncpnjba9ml.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://z5sxnxpgia.execute-api.us-east-1.amazonaws.com",
+  bff: "https://ncpnjba9ml.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
