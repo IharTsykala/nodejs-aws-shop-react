3b15141fa0110f8de3e0c08c3befef32e33c3e27
IharTsykala <ihatsykala@kitrum.com>
2023-06-20 02:40:43 +0600

    feat: add integration with s3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://ncpnjba9ml.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://98daqmq4jf.execute-api.eu-west-1.amazonaws.com",
   bff: "https://ncpnjba9ml.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
