53c635322f64704412d5c4ea55d1f1d252cdaf72
IharTsykala <ihatsykala@kitrum.com>
2023-06-27 02:20:46 +0600

    fix: api import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://ncpnjba9ml.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://98daqmq4jf.execute-api.us-east-1.amazonaws.com",
-  bff: "https://ncpnjba9ml.execute-api.us-east-1.amazonaws.com",
+  import: "https://0lfcftlssg.execute-api.us-east-1.amazonaws.com",
+  bff: "https://jmyszg25nh.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
